800e9cda0fbc4bebd1127e83037826ee1e93e8fc
Masanori Ohgita <piyo3728@gmail.com>
2012-12-28 16:41:47 +0900

    Add style_index.css

--- site/js/script.js
+++ site/js/script.js
@@ -10,6 +10,27 @@ $(function(){
 		Initialize
 	**/
 	function init() {
+		console.log("init()");
+		var $parent =  $("body");
+
+		/* Process for Content blocks ---------- */
+		
+		var $content_articles = $parent.children("article");
+		
+		/* Counting number of blocks (Lines) */
+		var block_num_y = $content_articles.length;
+
+		/* Counting number of blocks (Rows) */
+		var block_num_x = 0;
+		$content_articles.each(
+			function (i){
+				var $blocks = $(this).children("div");
+				if(block_num_x < $blocks.length){
+					block_num_x = $blocks.length;
+				}
+			}
+		);
+
 
 
 	};
