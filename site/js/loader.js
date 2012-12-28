bd20276322df58eef5e229af2a1570c04479cd55
Masanori Ohgita <piyo3728@gmail.com>
2012-12-28 15:41:52 +0900

    Include tags compressed to loader.js

---
+++ site/js/loader.js
@@ -0,0 +1,19 @@
+/**
+	Loader script for www.ohgita.info
+	(C) Masanori Ohgita.
+**/
+
+/* Load libraries and stylesheets, all at once. */
+
+var isMSIE = /*@cc_on!@*/false;
+if (isMSIE){
+	document.write('<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>');
+}
+
+document.write('<link rel="stylesheet" href="stylesheets/normalize.css" />');
+document.write('<link rel="stylesheet/less" type="text/css" href="stylesheets/style.less" />');
+
+document.write('<script type="text/javascript" src="/js/jquery-1.8.3.min.js"></script>');
+document.write('<script type="text/javascript" src="/js/less-1.3.1.min.js"></script>');
+
+document.write('<script type="text/javascript" src="/js/script.js"></script>');
---
+++ site/js/script.js
@@ -0,0 +1,18 @@
+/**
+	Script for www.ohgita.info
+	(C) Masanori Ohgita.
+**/
+
+/* On load */
+$(function(){
+
+	/**
+		Initialize
+	**/
+	function init() {
+
+
+	};
+
+	init();
+});
